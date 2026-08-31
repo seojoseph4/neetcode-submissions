04f36e8823a29dbd002a69d43c2edb651316f9f5
Joseph Seo <134018750+seojoseph4@users.noreply.github.com>
2026-08-30 23:38:21 -0400

    Add: course-schedule - submission-2

---
+++ Data Structures & Algorithms/course-schedule/submission-2.py
@@ -0,0 +1,29 @@
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        # build adjacency array
+        ad =defaultdict(list)
+
+        for post, prereq in prerequisites:
+            ad[prereq].append(post)
+        
+        seen = set()
+        seeing = set()
+        def dfs(currcourse):
+            seen.add(currcourse)
+            seeing.add(currcourse)
+            for nei in ad[currcourse]:
+                if nei in seeing:
+                    return False
+                if nei in seen:
+                    continue
+                
+                if dfs(nei) == False:
+                    return False
+            seeing.remove(currcourse)
+            return True
+        
+        for n in range(numCourses):
+            if n not in seen:
+                if dfs(n) == False:
+                    return False
+        return True
